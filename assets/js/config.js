184b97c1024c07b56db968b746197ce91da6b57b
shsussk <Francisperalta0416@gmail.com>
2026-02-19 16:18:54 -0400

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://pwebzodqtuqkormawqvy.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3ZWJ6b2RxdHVxa29ybWF3cXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MjUyMTksImV4cCI6MjA4NzEwMTIxOX0.ZuKeQZbIZ2njCfHILOdwAvibkfIfi8WK8mlQLPZYRyg"; // pega la key completa aquí
